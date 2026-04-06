5b0ee503c7144fc04dbcb2b75eaef4d2745cdd5d
JonesCG0 <153124541+JonesCG0@users.noreply.github.com>
2026-04-06 09:57:06 -0600

    Getting Ready for Azure

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee83eb0b2bd550c363164e5d389062a201fc9a8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d918099146ec2c2e5f278b542bdd0eee2dcc9133")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
