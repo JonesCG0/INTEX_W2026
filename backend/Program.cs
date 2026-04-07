6871e4d4b4ccd00f93145e9ccf26f17f31bb3269
JonesCG0 <153124541+JonesCG0@users.noreply.github.com>
2026-04-07 10:58:39 -0600

    Reorder Auth

--- backend/Program.cs
+++ backend/Program.cs
@@ -117,8 +117,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 app.UseCors("Frontend");
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
